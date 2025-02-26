36e46b819cef331ea47eaf79d71eb3b117de2534
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2025-02-27 00:41:30 +0530

    Construction Injection using Simple Example

--- Dependency Injection Practice/Program.cs
+++ Dependency Injection Practice/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public interface IEngine
+{
+    void Run();
+}
+
+public class PetrolEngine : IEngine
+{
+    public void Run() => Console.WriteLine("Petrol engine is running...");
+}
+
+public class DieselEngine : IEngine
+{
+    public void Run() => Console.WriteLine("Diesel engine is running...");
+}
+
+public class Car
+{
+    private readonly IEngine _engine;
+
+    public Car(IEngine engine) // ✅ Injecting Engine via Constructor
+    {
+        _engine = engine;
+    }
+
+    public void Start() => _engine.Run();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var car = new Car(new PetrolEngine());
+        car.Start();  // Output: Petrol engine is running...
+    }
+}
